f410629fc9b0a80089056b0daa79b914363f0c5b
OaklandHawk <hawks93187@gmail.com>
2018-07-16 16:08:52 -0700

    new commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-main.js
+//declare constants for textarea, button, and input
+var addPostInput = document.querySelector('input.addPostInput');
+var addTextArea = document.querySelector('textarea.addPostInput');
+var addPostButton = document.querySelector('button.addPostButton');
