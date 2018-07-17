39065e22b4ea9d869ca09550a95e2e2413f31e41
OaklandHawk <hawks93187@gmail.com>
2018-07-17 13:01:18 -0700

    new commit

--- js/main.js
+++ js/main.js
@@ -2,3 +2,18 @@
 var addPostInput = document.querySelector('input.addPostInput');
 var addTextArea = document.querySelector('textarea.addPostInput');
 var addPostButton = document.querySelector('button.addPostButton');
+
+//event listener for posting functionality
+addPostButton.addEventListener('click', function() {
+var body = document.getElementsByTagName('body')[0];
+
+var postBody = document.createElement('div');
+postBody.className = 'postBody';
+
+var header = document.createElement('h1');
+header.className = 'postHeader';
+header.textContent = addPostInput.value;
+
+var p = document.createElement('p');
+p.className = 'bodyContent';
+p.textContent = addTextArea.value;
